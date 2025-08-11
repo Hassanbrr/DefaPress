9f1b38cea16c47f70a4c218961e296af708e6804
Hassan_b <hasanbahmandar5@gmail.com>
2025-08-11 23:25:52 +0330

    Add project files.

---
+++ DefaPress.Repository/Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DefaPress.Repository.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+ 
+}
---
+++ DefaPress/Infrastructure/Extensions/ServicesExtensions.cs
@@ -0,0 +1,24 @@
+﻿using DefaPress.Repository.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace DefaPress.Presentation.Web.Infrastructure.Extensions
+{
+    public static class ServicesExtensions
+    {
+       
+        public static void AddCustomServicesToContainer(this IServiceCollection services, IConfiguration configuration)
+        {
+
+         
+
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+    
+ 
+        }
+
+    }
+}
